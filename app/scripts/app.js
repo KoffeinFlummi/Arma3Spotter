930862b1334e69a7b7094f11a4b626918513244e
Jo Dav <github@jonathandavid.de>
2014-10-22 22:55:19 +0200

    initial yeoman commit
    Run `npm install`
    Run `bower install`
    Run `grunt`

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name arma3SpotterApp
+ * @description
+ * # arma3SpotterApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('arma3SpotterApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name arma3SpotterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the arma3SpotterApp
+ */
+angular.module('arma3SpotterApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
