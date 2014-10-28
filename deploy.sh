3c395c689e57f1561293ced9d082011be269de7c
KoffeinFlummi <koffeinflummi@gmail.com>
2014-10-28 13:06:45 +0100

    Automated gh-pages deployment via Travis

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+rm -rf www || exit 0
+mkdir www
+cd src
+grunt build
+(
+  cd ../www
+  git init
+  git config user.name "Travis-CI"
+  git config user.email "travis@travis.travis"
+  git add .
+  git commit -m "Automated gh-pages deploy"
+  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+)
