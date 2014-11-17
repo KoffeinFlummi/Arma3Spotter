db74fefebbc15361a84ee2a67017a57f81dccb32
jodav <github@jonathandavid.de>
2014-11-17 08:04:40 +0100

    added globalisation plugin

--- platforms/android/ant-build/classes.dex.d
+++ platforms/android/ant-build/classes.dex.d
@@ -6,4 +6,7 @@
 /Users/dajo/Development/git/Arma3Spotter/platforms/android/ant-build/classes/com/agmmod/arma3spotter/R$string.class \
 /Users/dajo/Development/git/Arma3Spotter/platforms/android/ant-build/classes/com/agmmod/arma3spotter/R$xml.class \
 /Users/dajo/Development/git/Arma3Spotter/platforms/android/ant-build/classes/com/agmmod/arma3spotter/R.class \
+/Users/dajo/Development/git/Arma3Spotter/platforms/android/ant-build/classes/org/apache/cordova/globalization/Globalization$1.class \
+/Users/dajo/Development/git/Arma3Spotter/platforms/android/ant-build/classes/org/apache/cordova/globalization/Globalization.class \
+/Users/dajo/Development/git/Arma3Spotter/platforms/android/ant-build/classes/org/apache/cordova/globalization/GlobalizationError.class \
 /Users/dajo/Development/git/Arma3Spotter/platforms/android/ant-build/dexedLibs/classes-0fcc74a86df4f4c080c63668eec6bc21.jar \
