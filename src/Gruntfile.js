487d2b6ddc9766cf35c0ed2ad579658b1853b347
bux578 <github@jonathandavid.de>
2014-12-23 20:00:45 +0100

    make fonts work ... this time for sure!
    ;-(

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -190,7 +190,7 @@ module.exports = function (grunt) {
         importPath: "./bower_components",
         httpImagesPath: "/images",
         httpGeneratedImagesPath: "/images/generated",
-        httpFontsPath: "./styles/fonts",
+        httpFontsPath: "./fonts",
         relativeAssets: false,
         assetCacheBuster: false,
         raw: "Sass::Script::Number.precision = 10\n"
